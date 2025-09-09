4fbc9c62b6f3c894a33d39cae2e7be7ce5cc15a5
Lynx20wz <maks20wz@gmail.com>
2025-09-09 21:16:05 +0300

    Login page completed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
     import './styles/global.scss';
+    import {TolgeeProvider, Tolgee, DevTools, LanguageStorage} from '@tolgee/svelte';
+    import {FormatIcu} from "@tolgee/format-icu";
+
+
+    const tolgee = Tolgee()
+        .use(DevTools())
+        .use(FormatIcu())
+        .use(LanguageStorage())
+        .init({
+            availableLanguages: ['en', 'ru-RU'],
+            defaultLanguage: 'en',
+            apiUrl: import.meta.env.VITE_TOLGEE_API_URL,
+            apiKey: import.meta.env.VITE_TOLGEE_API_KEY,
+        });
 </script>
 
+<TolgeeProvider tolgee={tolgee}>
+  <main class="page">
+    <slot />
+  </main>
+</TolgeeProvider>
 
 <style lang="scss">
 
