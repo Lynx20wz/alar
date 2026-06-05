200f5192cafa369749e1e5677627c59041a455f9
Lynx20wz <maks20wz@gmail.com>
2026-06-05 22:06:24 +0300

    Added `object_id` field in exceptions

--- back/exceptions.py
+++ back/exceptions.py
@@ -5,28 +5,36 @@ class AppException(HTTPException):
     status_code: int = 400
     detail: str = 'Unexpected error'
 
-    def __init__(self, detail: str | None = None):
+    def __init__(self, detail: str | None = None, object_id: int | None = None):
+        self.object_id = object_id
         if detail:
             self.detail = detail
+        super().__init__(status_code=self.status_code, detail=self.detail)
 
 
 class NotFoundError(AppException):
     status_code: int = 404
+    object_id: int | None = None
+    detail: str = 'Not found'
 
 
 class UserNotFound(NotFoundError):
+    object_id: int | None = None
     detail: str = 'User not found'
 
 
 class PostNotFound(NotFoundError):
+    object_id: int | None = None
     detail: str = 'Post not found'
 
 
 class ImageNotFound(NotFoundError):
+    object_id: int | None = None
     detail: str = 'Image not found'
 
 
 class CommentNotFound(NotFoundError):
+    object_id: int | None = None
     detail: str = 'Comment not found'
 
 
